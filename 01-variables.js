e349a3fed0a16dc2c5c87a008781c7c3df7b8dc7
Ariel Marcelo <55715664+Ariel-Marcelo@users.noreply.github.com>
2022-11-14 17:55:45 -0500

    Variables js

---
+++ 01-variables.js
@@ -0,0 +1,30 @@
+// re-asignables
+var numeroUno = 1;
+let numeroDos = 2;
+
+numeroUno = 12;
+numeroDos = 8;
+
+numeroUno = false;
+numeroDos = true;
+
+// Imutables
+
+const configuracionArchivos = 'PDF';
+
+// Primitivas
+const numero = 1;
+const sueldo = 1.2;
+const texto = "Adri";
+const apellidos = "Eguez";
+const boolean = true;
+const hijos = null;
+const zapatos = undefined;
+
+console.log(typeof  numero )
+console.log(typeof  sueldo )
+console.log(typeof  texto )
+console.log(typeof  apellidos )
+console.log(typeof  boolean )
+console.log(typeof  hijos )
+console.log(typeof  zapatos )
