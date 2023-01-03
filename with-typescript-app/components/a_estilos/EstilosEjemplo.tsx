cf58ef8034593d0ca903efe72f35d567235b638b
Ariel Marcelo <55715664+Ariel-Marcelo@users.noreply.github.com>
2023-01-03 08:56:06 -0500

    styles and components in react

---
+++ with-typescript-app/components/a_estilos/EstilosEjemplo.tsx
@@ -0,0 +1,8 @@
+export function EstilosEjemplo () {
+  return (
+    <div>
+      <h1>Estilos en React</h1>
+      <p>Estilos en React</p>
+    </div>
+  )
+}
---
+++ with-typescript-app/pages/a_hola_mundo.tsx
@@ -0,0 +1,5 @@
+export default function a_hola_mundo() {
+  return (
+      <div> Hola Mundo </div>;
+  )
+}
